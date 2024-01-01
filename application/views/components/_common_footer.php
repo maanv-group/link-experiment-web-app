f58c1b2a7df1a4d934577145fbcc50d66b4e6660
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-01-02 00:31:02 +0530

    Footer

---
+++ application/views/components/_common_footer.php
@@ -0,0 +1,13 @@
+<div class="py-3 bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <?= date('Y') ?> &copy;<a href="https://github.com/HemantKarekar">Hemant Karekar</a> | All Rights Reserved. v1.1.0</div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Designed &amp; Developed by <a href="https://github.com/HemantKarekar" target="_blank" class="link"><i class="fa-brands fa-github"></i>&nbsp;Hemant Karekar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
