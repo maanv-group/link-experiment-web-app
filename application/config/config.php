d2138d8f179779ec3aed7808c8e099f4d95aeaae
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-01-02 00:13:03 +0530

    URL

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/mini-projects/bitly-me/';
+$config['base_url'] = 'https://app.hemantkarekar.com/qrlfixr/';
 
 /*
 |--------------------------------------------------------------------------
