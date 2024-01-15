e257b376e689be679e1ecd5461e26240c55f8936
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-01-15 11:57:36 +0530

    Dump in Office PC

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/mini-projects/bitly-me/';
-$config['base_url'] = 'https://app.hemantkarekar.com/qrlfixr/';
+$config['base_url'] = 'http://localhost:8000/bitly-me/';
+// $config['base_url'] = 'https://app.hemantkarekar.com/qrlfixr/';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -71,9 +71,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | the query builder class.
 */
 $active_group = 'production';
+$active_group = 'office';
 $query_builder = TRUE;
 
 $db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'maanv-app-bitly',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+$db['office'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'app-bitly',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'port' => 3300,
+	'save_queries' => TRUE
+);
+$db['testing'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
